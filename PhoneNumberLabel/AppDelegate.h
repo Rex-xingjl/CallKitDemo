d7c324c56a66547ba1761bc0095bf5785b55bb56
Rex <xingjl@outlook.com>
2017-08-02 10:57:36 +0800

    Initial Commit

---
+++ PhoneNumberLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhoneNumberLabel
+//
+//  Created by Rex@JJS on 2017/8/2.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PhoneNumberLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PhoneNumberLabel
+//
+//  Created by Rex@JJS on 2017/8/2.
+//  Copyright © 2017年 Rex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
